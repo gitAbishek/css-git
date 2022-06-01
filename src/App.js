8ec7e93c22605a4d60907a193e264910c427e97d
abishekGithub <abishekshah12315@gmail.com>
2022-06-01 21:09:17 +0545

    sample project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="container">
+     <div className="title">How To Make The Website Responsive</div>
+     <div className="grid">
+       <div className="grid1">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Cum culpa repellendus eius, perferendis qui quod totam harum,
+         commodi, soluta quam temporibus iusto. Neque blanditiis 
+         maxime reiciendis enim laborum earum accusamus. Lorem ipsum 
+         dolor sit amet consectetur adipisicing elit. Voluptatem dicta
+         minus nisi sequi, placeat ipsum possimus quasi excepturi sed 
+         consequatur, pariatur quae dolore quibusdam obcaecati eius 
+         ut alias veniam. Quae. Lorem ipsum, dolor sit amet consectetur 
+         adipisicing elit. Quam optio officiis fugiat nobis, repudiandae
+         similique suscipit? Officia, corrupti voluptas. Dolore eveniet
+         magni beatae minima cumque accusantium quasi expedita
+         voluptatem autem?
+       </div>
+       <div className="grid2">
+         <img src="image/image4.jpeg" alt="smallimage" />
+       </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
